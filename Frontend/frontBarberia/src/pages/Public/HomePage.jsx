7069aa4f486df22113680923cd015c2a883c91d6
Mauro Pardo <maupardo03@gmail.com>
2026-05-16 21:43:05 -0300

    Se agregó el logo del local, en la interfaz principal, como de fondo, sin mucha intensidad

--- Frontend/frontBarberia/src/pages/Public/HomePage.jsx
+++ Frontend/frontBarberia/src/pages/Public/HomePage.jsx
@@ -92,8 +92,26 @@ export default function HomePage() {
 
     return (
         <div className="relative min-h-screen overflow-hidden bg-zinc-950 py-12 px-4 sm:px-6 lg:px-8">
+            {/* Gradientes de fondo */}
             <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(circle_at_top,rgba(212,212,216,0.12),transparent_55%)]" />
             <div className="pointer-events-none absolute -right-24 top-24 h-72 w-72 rounded-full bg-zinc-700/10 blur-3xl" />
+            
+            {/* Logos de marca de agua - distribuidos en la página */}
+            <div className="pointer-events-none absolute top-20 left-10 opacity-[0.03] blur-[2px] grayscale">
+                <img src="/ferpa-logo.jpeg" alt="" className="w-32 h-32 sm:w-48 sm:h-48 lg:w-64 lg:h-64 object-contain rotate-[-15deg]" />
+            </div>
+            <div className="pointer-events-none absolute top-32 right-4 sm:right-16 opacity-[0.04] blur-[1px] grayscale">
+                <img src="/ferpa-logo.jpeg" alt="" className="w-24 h-24 sm:w-36 sm:h-36 xl:w-48 xl:h-48 object-contain rotate-[12deg]" />
+            </div>
+            <div className="pointer-events-none absolute bottom-32 left-4 sm:left-20 opacity-[0.03] blur-[2px] grayscale">
+                <img src="/ferpa-logo.jpeg" alt="" className="w-28 h-28 sm:w-40 sm:h-40 lg:w-56 lg:h-56 object-contain rotate-[8deg]" />
+            </div>
+            <div className="pointer-events-none absolute bottom-48 right-8 sm:right-12 opacity-[0.04] blur-[1px] grayscale hidden sm:block">
+                <img src="/ferpa-logo.jpeg" alt="" className="w-28 h-28 sm:w-32 sm:h-32 xl:w-40 xl:h-40 object-contain rotate-[-20deg]" />
+            </div>
+            <div className="pointer-events-none absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 opacity-[0.02] blur-[3px] grayscale">
+                <img src="/ferpa-logo.jpeg" alt="" className="w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 object-contain" />
+            </div>
 
             <div className="relative max-w-7xl mx-auto">
                 {/* Header */}
