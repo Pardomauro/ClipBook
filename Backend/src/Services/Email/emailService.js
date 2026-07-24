901baa79b22623e8ad2b55f68d0428765882b364
Mauro Pardo <maupardo03@gmail.com>
2026-07-23 23:51:33 -0300

    Se agregó el ultimo intento para forzar IPv4

--- Backend/src/Services/Email/emailService.js
+++ Backend/src/Services/Email/emailService.js
@@ -29,7 +29,8 @@ const verificarConexion = async () => {
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT) || 587,
-    secure: process.env.EMAIL_SECURE === 'true', // true para 465, false para otros puertos
+    secure: process.env.EMAIL_SECURE === 'false', // true para 465, false para otros puertos
+    family: 4, // <-- Le prohíbe explícitamente tocar IPv6
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
